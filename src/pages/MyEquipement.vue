e11365a4fb28e9cf73bd6958c527b2c45512a9ab
Raphael <pierrotraphael08@gmail.com>
2024-10-10 14:14:03 +0200

    creation de la page mes equipement

---
+++ src/pages/MyEquipement.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="flex flex-col bg-white w-full h-[746] mx-8 rounded-3xl">
+    <div class="flex flex-row justify-center py-8">
+      <button
+        class="text-[#2c4eff] text-[15px] font-normal font-['Inter'] px-6"
+      >
+        Défibrilateur
+      </button>
+      <button
+        class="text-[#5b5d68] text-[15px] font-normal font-['Inter'] px-6"
+      >
+        Extincteur
+      </button>
+      <button
+        class="text-[#5b5d68] text-[15px] font-normal font-['Inter'] px-6"
+      >
+        Autres
+      </button>
+    </div>
+    <div class="bg-[#f8f8f8] rounded-xl mx-4 shadow">
+      <div class="flex flex-row justify-evenly py-6">
+        <div
+          class="text-[#111928] text-[15px] font-medium font-['Inter'] leading-relaxed"
+        >
+          NAME
+        </div>
+        <div
+          class="text-[#111928] text-[15px] font-medium font-['Inter'] leading-relaxed"
+        >
+          NUMBER
+        </div>
+      </div>
+      <div class="bg-white flex flex-row justify-between p-4 rounded-xl pl-20">
+        <div
+          class="text-[#637381] text-sm font-normal font-['Inter'] leading-snug"
+        >
+          NAME
+        </div>
+        <div
+          class="text-[#637381] text-sm font-normal font-['Inter'] leading-snug"
+        >
+          NUMBER
+        </div>
+        <button
+          class="h-[34px] px-5 py-[5px] bg-white rounded-[50px] border border-[#3758f9] justify-center items-center inline-flex text-center text-[#3758f9] text-base font-medium font-['Inter'] leading-normal"
+        >
+          Edit
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "MyEquipement",
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,60 +1,65 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
-import AboutPage from '../pages/AboutPage.vue';
-import NotFound from '../pages/NotFound.vue';
-import SignIn from '../pages/SignIn.vue';
-import EmergencyKit from '../pages/EmergencyKit.vue'
-import ForgotPassword from '../pages/ForgotPassword.vue'
-import SignUp from '../pages/SignUp.vue'
+import AboutPage from "../pages/AboutPage.vue";
+import NotFound from "../pages/NotFound.vue";
+import SignIn from "../pages/SignIn.vue";
+import EmergencyKit from "../pages/EmergencyKit.vue";
+import ForgotPassword from "../pages/ForgotPassword.vue";
+import SignUp from "../pages/SignUp.vue";
 import AjoutEquipement from "../pages/AjoutEquipement.vue";
-import FicheEquipement from '../pages/FicheEquipement.vue'
+import FicheEquipement from "../pages/FicheEquipement.vue";
+import MyEquipement from "../pages/MyEquipement";
 const routes = [
- 
-    {
-      path: '/',
-      name: 'EmergencyKit',
-      component: EmergencyKit
-    },    
-    {
-      path:'/fiche-equipement',
-      name:'FicheEquipement',
-      component: FicheEquipement
-    }, 
-    {
-      path:'/ajout-equipement',
-      name:'AjoutEquipement',
-      component: AjoutEquipement
-    },   
-    {
-      path:'/forgotpassord',
-      name:'ForgotPassword',
-      component: ForgotPassword
-    }, 
-    {
-      path:'/signup',
-      name:'SignUp',
-      component: SignUp
-    },
-    {
-      path:'/signin',
-      name:'SignIn',
-      component: SignIn
-    },
-    {
-      path: '/about',
-      name: 'AboutPage',
-      component: AboutPage
-    },
-    {
-      path: '/:pathMatch(.*)*', // Gérer les routes non définies
-      name: 'NotFound',
-      component: NotFound
-    }
+  {
+    path: "/EmergencyKit",
+    name: "EmergencyKit",
+    component: EmergencyKit,
+  },
+  {
+    path: "/fiche-equipement",
+    name: "FicheEquipement",
+    component: FicheEquipement,
+  },
+  {
+    path: "/ajout-equipement",
+    name: "AjoutEquipement",
+    component: AjoutEquipement,
+  },
+  {
+    path: "/forgotpassord",
+    name: "ForgotPassword",
+    component: ForgotPassword,
+  },
+  {
+    path: "/signup",
+    name: "SignUp",
+    component: SignUp,
+  },
+  {
+    path: "/signin",
+    name: "SignIn",
+    component: SignIn,
+  },
+  {
+    path: "/about",
+    name: "AboutPage",
+    component: AboutPage,
+  },
+  {
+    path: "/MyEquipement",
+    name: "MyEquipement",
+    component: MyEquipement,
+  },
+  {
+    path: "/:pathMatch(.*)*", // Gérer les routes non définies
+    name: "NotFound",
+    component: NotFound,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-  });
+  history: createWebHistory(),
+  routes,
+});
 
 export default router;
